ba281fbe573c579f3bc835822f0ab55c582fef7b
sudhavalli <sudhavalli.parthasarathy@matrimony.com>
2025-04-01 15:47:27 +0530

    code modified for welcome msg

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 let express = require('express')
 let app = express();
-let msg = "welcome"
+let msg = "welcome to my demo"
 console.log("test");
 console.log("new line of code")
 app.listen(3000,()=>{console.log("server started")})
