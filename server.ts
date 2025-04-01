1f1c5e7dbb18cd18d42f06ae93bb70b326cac16f
sudhavalli <sudhavalli.parthasarathy@matrimony.com>
2025-04-01 15:43:06 +0530

    code modified for welcome msg

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 let express = require('express')
 let app = express();
 let msg = "welcome to git hub full stack demo"
-
+console.log("my new code on local")
 app.listen(3000,()=>{console.log("server started")})
